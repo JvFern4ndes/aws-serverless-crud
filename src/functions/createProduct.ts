b2a7965a533efa66ccd5c567d8f3b44f745a8d54
João <jvfernandes.contato@gmail.com>
2024-08-27 23:42:49 -0300

    feat: implement PutItemCommand from client-dynamosb sdk into the createProduct function

--- src/functions/createProduct.ts
+++ src/functions/createProduct.ts
@@ -1,13 +1,28 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { PutItemCommand } from '@aws-sdk/client-dynamodb';
 import type { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { randomUUID } from 'node:crypto';
+
+import { dynamoClient } from '../lib/dynamoClient';
 
 export async function handler(event: APIGatewayProxyEventV2) {
     const body = JSON.parse(event.body);
+
+    const id = randomUUID();
+
+    const command = new PutItemCommand({
+        TableName: 'ProductsTable',
+        Item: {
+            id: { S: id },
+            name: { S: body.name },
+            price: { N: `${body.price}` },
+            tags: { SS: body.tags },
+        },
+    });
+
+    const response = await dynamoClient.send(command);
     
     return {
         statusCode: 201,
-        body: JSON.stringify({
-            payloadRecebido: body,
-        }),
+        body: JSON.stringify(response),
     };
 }
