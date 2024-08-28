4a6b624172c8956da07ece5c1f6ddf97b73d31ca
João <jvfernandes.contato@gmail.com>
2024-08-27 23:21:47 -0300

    feat: adding the client-dynamodb sdk and create the client to interact with dynamoDB

--- src/functions/createProduct.ts
+++ src/functions/createProduct.ts
@@ -1,6 +1,13 @@
-export async function handler() {
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import type { APIGatewayProxyEventV2 } from 'aws-lambda';
+
+export async function handler(event: APIGatewayProxyEventV2) {
+    const body = JSON.parse(event.body);
+    
     return {
         statusCode: 201,
-        body: 'createProduct',
-    }
+        body: JSON.stringify({
+            payloadRecebido: body,
+        }),
+    };
 }
