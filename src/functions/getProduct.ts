47d7e0332825b4f0992997682171e392ea72a75d
João <jvfernandes.contato@gmail.com>
2024-09-02 11:21:50 -0300

    feat: create a delete function

--- src/functions/getProduct.ts
+++ src/functions/getProduct.ts
@@ -1,5 +1,5 @@
 import { GetCommand } from "@aws-sdk/lib-dynamodb";
-import { APIGatewayProxyEventV2 } from "aws-lambda";
+import type { APIGatewayProxyEventV2 } from "aws-lambda";
 import { dynamoClient } from "src/lib/dynamoClient";
 
 export async function handler(event: APIGatewayProxyEventV2) {
